907be157eec80b0e48172ab0ca55afbaa8398384
Anna Axelsson <a.axelsson51@gmail.com>
2023-02-19 19:37:24 +0100

    Alter tests and add method

--- src/main/java/com/example/Calculator.java
+++ src/main/java/com/example/Calculator.java
@@ -1,13 +1,18 @@
 package com.example;
 
+import java.util.List;
+
 public class Calculator {
 
-    public int add(int number, int number1, int... others) {
-        int firstSum = number + number1;
-        for (int i : others) {
-            firstSum += i;
+    public int add(List<Integer> items) {
+        if (items != null) { //throw exception
+            Integer firstSum = 0;
+            for (int i : items) {
+                firstSum += i;
+            }
+            return firstSum;
+        }else{
+            throw new RuntimeException("Can not be null");
         }
-        return firstSum;
     }
-    //commit: steg 2
 }
--- src/test/java/com/example/CalculatorTest.java
+++ src/test/java/com/example/CalculatorTest.java
@@ -1,18 +1,25 @@
 package com.example;
 
+import org.assertj.core.util.Lists;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
 
 public class CalculatorTest {
     private Calculator calculator = new Calculator(); //steg 1, 2 och 3
 
-@Test
-    public void testingAddingMoreNumbers() {
-    Assertions.assertEquals(10, calculator.add(2, 3, 5));
-}
-@Test
-    public void testingAddingMoreNumbersMoreItems(){
-        Assertions.assertEquals(15, calculator.add(2,3,5,5));
-        }
-        //Commit: steg 2
+    @Test
+    public void testAddElementsFromList(){
+        Assertions.assertEquals(10, calculator.add(Lists.list(2,3,4,1)));
+    }
+    @Test
+    public void testAddElementsFromEmptyListShouldReturn0(){
+        Assertions.assertEquals(0, calculator.add(new ArrayList<>()));
+    }
+    @Test
+    public void testAddingElementsFromNullShouldThrowException(){
+        Assertions.assertThrows(RuntimeException.class, () -> calculator.add(null));
+//Commit steg 3
+    }
 }
